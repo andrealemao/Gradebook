a523e68f54f13e12dc631b41d951c868fa537257
andreluizalemao@gmail.com <@Ndre1990>
2021-09-27 14:07:32 -0300

    first commit

---
+++ reallife-aplication/modules/gradebook/gradebook-web/src/main/java/com/liferay/training/gradebook/web/portlet/GradebookPortlet.java
@@ -0,0 +1,27 @@
+package com.liferay.training.gradebook.web.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.training.gradebook.web.constants.GradebookPortletKeys;
+import org.osgi.service.component.annotations.Component;
+
+import javax.portlet.Portlet;
+
+/**
+ * @author andre.goulart
+ */
+@Component(
+    immediate = true,
+    property = {
+        "com.liferay.portlet.display-category=category.training",
+        "com.liferay.portlet.instanceable=false",
+        "javax.portlet.display-name=Gradebook",
+        "javax.portlet.init-param.template-path=/",
+        "javax.portlet.init-param.view-template=/view.jsp",
+        "javax.portlet.name=" + GradebookPortletKeys.GRADEBOOK,
+        "javax.portlet.resource-bundle=content.Language",
+        "javax.portlet.security-role-ref=power-user,user"
+    },
+    service = Portlet.class
+)
+public class GradebookPortlet extends MVCPortlet {
+}
